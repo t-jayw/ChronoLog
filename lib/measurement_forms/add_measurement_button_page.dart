99aabfff78564925b96bdfa33cef6bdb2e6974f4
tylerw <tylerjaywood@gmail.com>
2024-03-28 22:15:34 -0600

    moving into configurable picker, 24 hour not working yet

--- lib/measurement_forms/add_measurement_button_page.dart
+++ lib/measurement_forms/add_measurement_button_page.dart
@@ -1,3 +1,4 @@
+import 'package:chronolog/components/measurement/configurable_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ulid/ulid.dart';
@@ -50,7 +51,6 @@ class _AddMeasurementButtonPageState extends State<AddMeasurementButtonPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Measurement',
@@ -81,10 +81,15 @@ class _AddMeasurementButtonPageState extends State<AddMeasurementButtonPage> {
               key: _formKey,
               child: Column(
                 children: [
-                  TapMeasurementTimePicker(
+                  // TapMeasurementTimePicker(
+                  //   onTimeChanged: _updateTime,
+                  //   initialTime:
+                  //       timeForPicker, // Pass the initial value to the picker
+                  // ),
+                  ConfigurablePrecisionTimePicker(
                     onTimeChanged: _updateTime,
-                    initialTime:
-                        timeForPicker, // Pass the initial value to the picker
+                    initialTime: timeForPicker,
+                    mode: TimePickerMode.tap,
                   ),
                   // Tag selection section
                   TagSelector(
@@ -125,7 +130,7 @@ class _AddMeasurementButtonPageState extends State<AddMeasurementButtonPage> {
 
                           timingMeasurementListProvider
                               .addTimingMeasurement(measurement);
-                              _showSnackBar();
+                          _showSnackBar();
                           Navigator.pop(context);
                         },
                       ),
