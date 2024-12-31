c74d9ab57dcddb730974407bbbbe76b14c3c3db5
tylerw <tylerjaywood@gmail.com>
2024-12-31 12:28:00 -0700

    cleaned up old pods or something appstore

--- lib/screens/info_page_screen.dart
+++ lib/screens/info_page_screen.dart
@@ -65,7 +65,7 @@ void sendMailWithFeedback() async {
 class InfoPage extends ConsumerWidget {
   InfoPage({Key? key}) : super(key: key);
 
-  final String versionNumber = "1.6.0";
+  final String versionNumber = "1.6.1";
   // replace with actual value
 
   final DatabaseHelper _db = DatabaseHelper();
